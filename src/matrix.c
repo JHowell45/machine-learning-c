8b88cb33a40df2c6b0ab5e076e0a4481679f849e
Jacob <jacob@notactivelylooking.com>
2024-08-15 00:50:37 +0100

    Updated how the matrices are created.

--- src/matrix.c
+++ src/matrix.c
@@ -4,21 +4,25 @@
 #include "vector.h"
 #include "matrix.h"
 
-matrix_t *newMatrix(size_t rows, size_t columns, double *data) {
+matrix_t *newMatrix(size_t columns, size_t rows, double *data) {
     printf("data: %lf\n", data[3]);
     matrix_t *matrix = malloc(sizeof(matrix_t));
     matrix->rows = rows;
     matrix->columns = columns;
-    matrix->items = calloc(matrix->columns, calloc(matrix->rows, sizeof(double)));
-    for (int x = 0; x < columns; x++) {
-        for (int y = 0; y < rows; y++) {
-            matrix->items[y][x] = data[(x + 1) * y];
+    matrix->items = calloc(matrix->columns, sizeof(double *));
+    for (int y = 0; y < matrix->columns; y++) {
+        matrix->items[y] = calloc(matrix->rows, sizeof(double));
+        for (int x = 0; x < matrix->rows; x++) {
+            size_t index = (y * matrix->columns + y) + x;
+            printf("INDEX: %zu || X: %d, Y: %d, Y2: %zu\n", index, x, y, (y * matrix->columns + y));
+            matrix->items[y][x] = data[index];
         }
     }
     return matrix;
 }
 
 void matrixPrint(matrix_t *matrix) {
+    printf("HELLO");
     for (int x = 0; x < matrix->columns; x++) {
         for (int y = 0; y < matrix->rows; y++) {
             printf("%lf, ", matrix->items[y][x]);
