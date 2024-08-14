533809e52311263dd8e60828f3ec902d34918b07
Jacob <jacob@notactivelylooking.com>
2024-08-15 00:27:15 +0100

    basic impl.

--- src/matrix.c
+++ src/matrix.c
@@ -1,17 +1,30 @@
+#include <stdio.h>
 #include <stdlib.h>
 
 #include "vector.h"
 #include "matrix.h"
 
-matrix_t *newMatrix(size_t rows, size_t columns) {
+matrix_t *newMatrix(size_t rows, size_t columns, double *data) {
+    printf("data: %lf\n", data[3]);
     matrix_t *matrix = malloc(sizeof(matrix_t));
-    matrix->columns = columns;
     matrix->rows = rows;
-    matrix->items = calloc(matrix->columns, sizeof(vector_t));
+    matrix->columns = columns;
+    matrix->items = calloc(matrix->columns, calloc(matrix->rows, sizeof(double)));
+    for (int x = 0; x < columns; x++) {
+        for (int y = 0; y < rows; y++) {
+            matrix->items[y][x] = data[(x + 1) * y];
+        }
+    }
+    return matrix;
 }
 
-void matrixInsertColumn(matrix_t *matrix, vector_t *column, size_t columnIndex) {
-    matrix->items[columnIndex] = column;
+void matrixPrint(matrix_t *matrix) {
+    for (int x = 0; x < matrix->columns; x++) {
+        for (int y = 0; y < matrix->rows; y++) {
+            printf("%lf, ", matrix->items[y][x]);
+        }
+        printf("\n");
+    }
 }
 
 void freeMatrix(matrix_t *matrix) {
