afbcb990048fd9de826986313a6e77a4930081a9
Jacob <jacob@notactivelylooking.com>
2024-08-14 21:45:25 +0100

    Updated vec impl to be more in line with maths examples.

--- include/vector.h
+++ include/vector.h
@@ -6,19 +6,10 @@
 
 typedef struct {
     size_t length;
-    size_t capacity;
     double **items;
 } vector_t;
 
-vector_t *newVec(size_t capacity);
-
-void vectorPush(vector_t *vec, double item);
-
-void vectorInsert(vector_t *vec, double item, size_t index);
-
-void vectorPop(vector_t *vec);
-
-void vectorRemove(vector_t *vec, size_t index);
+vector_t *newVector(double items[], size_t length);
 
 void freeVector(vector_t *vec);
 
--- src/vector.c
+++ src/vector.c
@@ -2,43 +2,14 @@
 
 #include "vector.h"
 
-vector_t *newVec(size_t capacity) {
+vector_t *newVector(double items[], size_t length) {
     vector_t *vec = malloc(sizeof(vector_t));
-    vec->length = 0;
-    vec->capacity = capacity;
-    vec->items = calloc(vec->capacity, sizeof(double));
+    vec->length = length;
+    vec->items = calloc(vec->length, sizeof(double));
+    for (int i = 0; i < vec->length; i++) {
+        vec->items[i] = &items[i];
+    }
     return vec;
-}
-
-void vectorIncreaseCapacity(vector_t *vec) {
-    vec->capacity *= 2;
-    realloc(vec->items, vec->capacity);
-}
-
-void vectorPush(vector_t *vec, double item) {
-    if (vec->length >= vec->capacity - 1) {
-        vectorIncreaseCapacity(vec);
-    }
-    vec->items[vec->length] = &item;
-    vec->length++;
-}
-
-void vectorInsert(vector_t *vec, double item, size_t index) {
-    if (vec->length >= vec->capacity - 1) {
-        vectorIncreaseCapacity(vec);
-    }
-    for (int insertIndex = vec->length-1; insertIndex >= index; index--) {
-        vec->items[index+1] = vec->items[index];
-    }
-    vec->items[index] = &item;
-}
-
-void vectorPop(vector_t *vec) {
-    vec->length--;
-}
-
-void vectorRemove(vector_t *vec, size_t index) {
-
 }
 
 void freeVector(vector_t *vec) {
