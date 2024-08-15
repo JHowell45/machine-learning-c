3a5106733e33fb44b5bd456506d31c99199efc94
Jacob <jacob@notactivelylooking.com>
2024-08-15 01:34:57 +0100

    Updating name to make it easier to understand.

--- examples/test_multi_arrays.c
+++ examples/test_multi_arrays.c
@@ -2,26 +2,26 @@
 #include <stdio.h>
 #include "matrix.h"
 
-#define ARRAY_2D_X 4
-#define ARRAY_2D_Y 3
+#define ARRAY_2D_COLS 4
+#define ARRAY_2D_ROWS 3
 
 
 int main(void) {
-    double data[ARRAY_2D_Y][ARRAY_2D_X] = {
+    double data[ARRAY_2D_ROWS][ARRAY_2D_COLS] = {
         {1,2,3,4},
         {5,6,7,8},
         {9,10,11,12}
     };
 
-    for (int y = 0; y < ARRAY_2D_Y; y++) {
-        for (int x = 0; x < ARRAY_2D_X; x++) {
+    for (int y = 0; y < ARRAY_2D_ROWS; y++) {
+        for (int x = 0; x < ARRAY_2D_COLS; x++) {
             printf("%lf, ", data[y][x]);
         }
         printf("\n");
     }
     printf("\n");
 
-    matrix_t *matrix = newMatrix(ARRAY_2D_Y, ARRAY_2D_X, data);
+    matrix_t *matrix = newMatrix(ARRAY_2D_ROWS, ARRAY_2D_COLS, data);
     matrixPrint(matrix);
     freeMatrix(matrix);
     return 0;
